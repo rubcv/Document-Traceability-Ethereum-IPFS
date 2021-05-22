dcdc9874fc061cc2452c8ac1cd46e29226b4fd1e
rubcv <spamdesareth@gmail.com>
2021-05-22 10:21:27 +0200

    memory string

--- ethereum/contracts/Document.sol
+++ ethereum/contracts/Document.sol
@@ -1,5 +1,13 @@
 pragma solidity 0.5.16;
 
 contract StoreDocument {
-    
+    string ipfsHash;
+
+    function setHash(string memory _hash) public {
+        ipfsHash = _hash;
+    }
+
+    function getHash() public {
+        return ipfsHash;
+    }
 }
