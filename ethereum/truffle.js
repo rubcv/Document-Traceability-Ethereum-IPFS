7fb1dbab40172998b6a5980e7cfff23e8f0e0d5f
rubcv <spamdesareth@gmail.com>
2021-05-22 10:12:22 +0200

    ganache config and fixed solidity compiler version

--- ethereum/truffle.js
+++ ethereum/truffle.js
@@ -2,11 +2,8 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 7545, // Ganache port
       network_id: "*" // Match any network id
-    },
-    develop: {
-      port: 8545
     }
   }
 };
